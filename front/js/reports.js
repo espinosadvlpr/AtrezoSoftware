3cf74428ddde58cc77c285112114f7f2d090ae29
espinosadvlpr <espinosadavid47@gmail.com>
2022-03-26 23:19:58 -0500

    (add module): create report module)

---
+++ front/js/reports.js
@@ -0,0 +1,58 @@
+window.onload = validateUser();
+
+function validateUser(){
+    if(window.sessionStorage){
+        var user = sessionStorage.getItem("user")
+        if(user == null){
+            window.location = '../index.html'
+        }else{
+            getUser()
+        }
+    }
+}
+
+function getUser() {
+    var h1 = document.getElementById("User")
+    if (window.sessionStorage) {
+        var user = sessionStorage.getItem("user");
+        var bienvenida = "Hola " + user + " ¿Que reportes deseas generar hoy?"
+        h1.textContent = bienvenida
+            //sessionStorage.removeItem("user");
+    }
+}
+
+// pendiente metodos para generar los reportes
+
+// Load the Visualization API and the corechart package.
+      google.charts.load('current', {'packages':['corechart']});
+
+      // Set a callback to run when the Google Visualization API is loaded.
+      google.charts.setOnLoadCallback(drawChart);
+
+      // Callback that creates and populates a data table,
+      // instantiates the pie chart, passes in the data and
+      // draws it.
+
+function drawChart() {
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Topping');
+        data.addColumn('number', 'Slices');
+        data.addRows([
+          ['Mushrooms', 3],
+          ['Onions', 1],
+          ['Olives', 1],
+          ['Zucchini', 1],
+          ['Pepperoni', 2]
+        ]);
+
+        // Set chart options
+        var options = {'title':'How Much Pizza I Ate Last Night',
+                       'width':400,
+                       'height':300};
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+}
